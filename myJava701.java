36dc8a1e64372561f416302b6574cebfedf8cb73
nameps <50565475+nameps@users.noreply.github.com>
2020-08-18 17:24:20 +0700

    Add files via upload

---
+++ myJava701.java
@@ -0,0 +1,18 @@
+package week7b;
+
+public class myJava701 {
+	public static void main(String[]args) {
+		char text1='M';
+		char text2='A';
+		char text3='B';
+		char aStart ='*';
+		char text4='4';
+		System.out.println("Hello\nther");
+		System.out.println("text1\\b" +text1);
+		System.out.println("text2\t" +text2);
+		System.out.println("aStart\r" +aStart);
+		System.out.println("text3\\" +text3);
+		System.out.println("text4\''" +text4);
+		
+	}
+}
---
+++ myJava702.java
@@ -0,0 +1,22 @@
+package week7b;
+
+public class myJava702 {
+	public static void main(String[]args) {
+		int a=5;
+		int b=10;
+		int c=20;
+		int total=a+b;
+		int total2=b/a;
+		int total3=a*c;
+		int total4=a%c;
+		
+		float  total5=a*a/b-c;
+		System.out.println("Total1 =" + total);
+		System.out.println("Total2 =" + total2);
+		System.out.println("Total3 =" + total3);
+		System.out.println("Total4 =" + total4);
+		System.out.println("Total5 =" + total5);
+		
+	}
+
+}
